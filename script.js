6d1be29d29f7369e01ce3e2d64fc29f462e5c551
AdrienG0 <145970282+AdrienG0@users.noreply.github.com>
2024-05-20 21:04:42 +0200

    LocalStorage opgelost
    Wat moeilijkheden maar een ander manier gevonden met script.js

--- script.js
+++ script.js
@@ -1,33 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-
     const registrationForm = document.getElementById("registrationForm");
-    registrationForm.addEventListener("submit", function(event) {
+    if (registrationForm) {
+        registrationForm.addEventListener("submit", function() {
+            const username = document.getElementById("name").value;
+            localStorage.setItem("username", username);
+        });
+    }
 
-        if (!registrationForm.checkValidity()) {
-            event.preventDefault();
-            return false;
-        }
-
-        const usernameInput = document.getElementById("name");
-        const username = usernameInput.value;
-        localStorage.setItem("username", username);
-
-        window.location.href = "receptenboek.html";
-    });
-
-
-    const loginForm = document.getElementById("loginForm"); 
-    loginForm.addEventListener("submit", function(event) {
-
-        if (!loginForm.checkValidity()) { 
-            event.preventDefault();
-            return false;
-        }
-
-        const emailInput = document.getElementById("email");
-        const username = emailInput.value;
-        localStorage.setItem("username", username);
-
-        window.location.href = "receptenboek.html";
-    });
+    const loginForm = document.getElementById("loginForm");
+    if (loginForm) {
+        loginForm.addEventListener("submit", function() {
+            const email = document.getElementById("email").value;
+            localStorage.setItem("username", email);
+        });
+    }
 });
