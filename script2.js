f46853e47ba83884a90cab3760223ae0402cef64
AdrienG0 <145970282+AdrienG0@users.noreply.github.com>
2024-05-21 21:42:05 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,7 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", () => {
     const usernameSpan = document.getElementById("username");
     const storedUsername = localStorage.getItem("username");
     if (storedUsername) {
         usernameSpan.textContent = storedUsername;
     }
+
+    const recipeForm = document.getElementById("recipeForm");
+    const recipeList = document.getElementById("recipeList");
+
+    const recipes = JSON.parse(localStorage.getItem("recipes")) || [];
+
+    
 });
