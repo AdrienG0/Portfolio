e136987f7c2a25a004129dd553da5eb66e95a820
AdrienG0 <145970282+AdrienG0@users.noreply.github.com>
2024-05-30 14:38:17 +0100

    Structuur aanbrengen

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const registrationForm = document.getElementById("registrationForm");
+    const registrationForm = document.getElementById("registratieFormulier");
     if (registrationForm) {
         registrationForm.addEventListener("submit", function() {
             const username = document.getElementById("name").value;
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
-    const loginForm = document.getElementById("loginForm");
+    const loginForm = document.getElementById("loginFormulier");
     if (loginForm) {
         loginForm.addEventListener("submit", function() {
             const email = document.getElementById("email").value;
